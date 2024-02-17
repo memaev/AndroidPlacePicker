0046027d26dbe1a55ab0b1819c494422c13050db
Mikhail Maevskiy <57516381+memaev@users.noreply.github.com>
2024-02-17 18:31:14 +0200

    Update

--- placepicker/build.gradle.kts
+++ placepicker/build.gradle.kts
@@ -73,7 +73,7 @@ publishing{
         create<MavenPublication>("release"){
             groupId = "dem.llc"
             artifactId = "placepicker"
-            version = "1.0.0"
+            version = "1.0.1"
 
             artifact("$buildDir/outputs/aar/placepicker-release.aar")
 
