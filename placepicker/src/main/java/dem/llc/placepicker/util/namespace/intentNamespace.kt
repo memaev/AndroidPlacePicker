0cabfb840df00957d890e7c33dd68349a1c09743
Mikhail Maevskiy <57516381+memaev@users.noreply.github.com>
2024-02-19 13:18:37 +0200

    Removed useless

--- placepicker/src/main/java/dem/llc/placepicker/util/namespace/intentNamespace.kt
+++
@@ -1,4 +0,0 @@
-package dem.llc.placepicker.util.namespace
-
-const val API_KEY = "api_key"
-const val LOCATION = "location"
--- sampleapp/src/main/java/dem/llc/androidplacepicker/presentation/viewModel/MainActivityViewModel.kt
+++
@@ -1,8 +0,0 @@
-package dem.llc.androidplacepicker.presentation.viewModel
-
-import androidx.compose.runtime.mutableStateOf
-import androidx.lifecycle.ViewModel
-
-class MainActivityViewModel : ViewModel() {
-    val result = mutableStateOf("No result")
-}
