d66550118f0024a514d82481079c88bae63ac5b8
Mikhail Maevskiy <57516381+memaev@users.noreply.github.com>
2024-03-28 01:57:59 +0200

    Updated name. AddressManager -> LocationManager

--- placepicker/src/main/java/dem/llc/placepicker/domain/manager/LocationManager.kt
+++ placepicker/src/main/java/dem/llc/placepicker/domain/manager/LocationManager.kt
@@ -1,10 +1,9 @@
 package dem.llc.placepicker.domain.manager
 
-import android.location.Geocoder
 import dem.llc.placepicker.domain.entity.Point
 import dem.llc.placepicker.domain.util.Result
 
-interface AddressManager {
+interface LocationManager {
     suspend fun getAddressName(point: Point): Result
     suspend fun getLocationByName(name: String): Result
 }
