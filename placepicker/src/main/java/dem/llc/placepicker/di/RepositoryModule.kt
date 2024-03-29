f2ff725eaf707e731001c083ff98da375d2cd416
Mikhail Maevskiy <57516381+memaev@users.noreply.github.com>
2024-03-29 16:31:38 +0300

    Added repository module

--- placepicker/src/main/java/dem/llc/placepicker/di/RepositoryModule.kt
+++ placepicker/src/main/java/dem/llc/placepicker/di/RepositoryModule.kt
@@ -1,11 +1,16 @@
 package dem.llc.placepicker.di
 
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import dem.llc.placepicker.data.managerImpl.LocationManagerImpl
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object RepositoryModule {
-
+class RepositoryModule {
+    @Provides
+    @Singleton
+    fun provideLocationManager() = LocationManagerImpl()
 }
