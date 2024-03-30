7207f86831a6dcfc596fe98dd525196aec6a293c
Kahilo Massango <kahilo42@gmail.com>
2024-03-30 13:36:33 +0100

    Remove duplicated androidTestImplementation

--- sampleapp/build.gradle.kts
+++ sampleapp/build.gradle.kts
@@ -77,7 +77,6 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
